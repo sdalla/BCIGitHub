801a1b9d3eeb97266a494be61a3d1844696e4862
Shiv Dalla <sdalla@seas.upenn.edu>
2018-04-16 15:21:52 -0400

    post OH
    5999 windows is right, just remove the last value of decimate to match.
    also, in spectrogram we needed to add the overlap argument, looks
    correct TA said

--- Checkpoint1_v1.m
+++ Checkpoint1_v1.m
@@ -29,7 +29,7 @@ winDisp = .05;
 %subject 1
 sub1tdv = cell(1,62);
 for i = 1:62
-   sub1tdv{i} = MovingWinFeats(Sub1_Training_ecog{1,i,1}, fs, winLen, winDisp, tdvFxn);
+   sub1tdv{i} = MovingWinFeats(Sub1_training_ecog{1,i,1}, fs, winLen, winDisp, tdvFxn);
 end
 %% Feature Extraction (Average Frequency-Domain Magnitude in 5 bands)
 % Frequency bands are: 5-15Hz, 20-25Hz, 75-115Hz, 125-160Hz, 160-175Hz
@@ -38,17 +38,18 @@ window = winLen*fs;
 
 %subject 1
 for i = 1:62
-[s,f,t] = spectrogram(Sub1_Training_ecog{1,1,1},window,[],[],fs);
-sub1f5_15{i} = mean(s(f(f>5)<15,:));
-sub1f20_25{i} = mean(s(f(f>20)<25,:));
-sub1f75_115{i} = mean(s(f(f>75)<115,:));
-sub1f125_160{i} = mean(s(f(f>125)<160,:));
-sub1f160_175{i} = mean(s(f(f>160)<175,:));
+[s,f,t] = spectrogram(Sub1_training_ecog{1,1,1},window,winDisp*fs,[],fs);
+    sub1f5_15{i} = mean(s(f(f>5)<15,:));
+    sub1f20_25{i} = mean(s(f(f>20)<25,:));
+    sub1f75_115{i} = mean(s(f(f>75)<115,:));
+    sub1f125_160{i} = mean(s(f(f>125)<160,:));
+    sub1f160_175{i} = mean(s(f(f>160)<175,:));
 end
 
 %% Decimation of dataglove
 load('Sub1_Training_dg.mat');
 % decimated glove data for subject one
+% take out the last value to match our 5999
 sub1DataGlove = cell(1,5);
 for i = 1:5
     sub1DataGlove{i} = decimate(Sub1_Training_dg{i},50);
