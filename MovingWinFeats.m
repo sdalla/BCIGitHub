48fc1f8980d21400fbb4273433251042d7832208
Shira Rieke <srieke@seas.upenn.edu>
2018-04-15 23:02:01 -0400

    tdv

--- MovingWinFeats.m
+++ MovingWinFeats.m
@@ -2,7 +2,7 @@ function out = MovingWinFeats(x, fs, winLen, winDisp, featFn)
 wins = length(0:winDisp*fs:length(x))-(winLen/winDisp);
 
     for i = 1:wins
-        data = x(1+(i-1)*winDisp*fs:(fs*winDisp*(i-1)+winLen*fs));
+        data = x(round(1+(i-1)*winDisp*fs):round((fs*winDisp*(i-1)+winLen*fs)));
         out(i) = featFn(data);
     end
 end
