25c0c15e904d74973e8aa1c2163d32a5ffea5d84
Shiv Dalla <sdalla@seas.upenn.edu>
2018-04-23 21:28:31 -0400

    make filterCoeffs and started feat extraction

--- Liang_2012_approach.m
+++ Liang_2012_approach.m
@@ -17,7 +17,7 @@ load('sub1_Training_dg.mat');
 load('sub1DataGlove.mat');
 
 
-%% break into freq bands using spectrogram
+%% break into freq bands using spectrogram, could do next section instead
 
 window = winLen*fs;
 freq_arr = 0:1:500; 
@@ -30,4 +30,19 @@ for i = 1:62
     
 end
 
-%%
+%% break into freq bands using FIR filters and filterDesigner
+for i = 1:62   
+    sub1_1_60filt{i} = filtfilt(Filter1,1,Sub1_Training_ecog{1,i});
+    sub1_60_100filt{i} = filtfilt(Filter2,1,Sub1_Training_ecog{1,i});
+    sub1_100_200filt{i} = filtfilt(Filter3,1,Sub1_Training_ecog{1,i});
+end
+
+%% plot the filtered and original data (sanity check)
+subplot(4,1,1)
+plot(Sub1_Training_ecog{1,1}(1:10000))
+subplot(4,1,2)
+plot(sub1_1_60filt{1}(1:10000))
+subplot(4,1,3)
+plot(sub1_60_100filt{1}(1:10000))
+subplot(4,1,4)
+plot(sub1_100_200filt{1}(1:10000))
