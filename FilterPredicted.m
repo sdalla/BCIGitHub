665df8f5a88a002c5ea7c971dcc824a6f5af3061
Shira Rieke <srieke@seas.upenn.edu>
2018-04-25 11:56:42 -0400

    plotting filter prediction outputs

--- FilterPredicted.m
+++ FilterPredicted.m
@@ -4,11 +4,29 @@ for i = 1:3
         output{i}(:,j) = medfilt1(predicted_dg{i}(:,j),1000);
     end
 end
-
-figure()
+%%
+figure(1)
 plot(predicted_dg{1}(:,1))
 hold on
 plot(output{1}(:,1))
 legend('No filter','Filter')
-predicted_dg = output;
-save predicted_dg_filt predicted_dg
+
+figure(2)
+plot(predicted_dg{1}(:,2))
+hold on
+plot(output{1}(:,2))
+legend('No filter','Filter')
+
+figure(3)
+plot(predicted_dg{1}(:,3))
+hold on
+plot(output{1}(:,3))
+legend('No filter','Filter')
+
+figure(5)
+plot(predicted_dg{1}(:,5))
+hold on
+plot(output{1}(:,5))
+legend('No filter','Filter')
+%predicted_dg = output;
+%save predicted_dg_filt predicted_dg
